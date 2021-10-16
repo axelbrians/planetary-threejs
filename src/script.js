6b6a08a183e572530f62207142a0f92f22f4868c
axelbrians <axelbrians@gmail.com>
2021-10-17 00:59:51 +0700

    initial commit

---
+++ src/script.js
@@ -0,0 +1,205 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
+import MoonTexture from './assets/images/MoonTexture.jpg';
+import EarthTexture from './assets/images/EarthTexture.jpg';
+import EarthCloud from './assets/images/EarthCloud.png';
+import SunTexture from './assets/images/SunTexture.jpg';
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+
+// Scene
+const scene = new THREE.Scene();
+
+
+// Grid helper
+// const gridHelper = new THREE.GridHelper( 1000, 1000 );
+// scene.add( gridHelper );
+
+
+// Geomtry
+
+
+
+// Mesh
+
+
+
+// Add mesh to canvass
+
+
+function generateStar() {
+    var positions = Array.from(
+        {length: 3}, () => {
+        var plusOrMin = Math.round(Math.random()) * 2 - 1;
+        // console.log(plusOrMin);
+        return Math.floor(Math.random() * 1000) * plusOrMin
+        }
+    );
+
+    // console.log("added new star" + positions);
+    var star = new THREE.SphereGeometry( 1, 32, 16 );
+    var starMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: false } );
+    var starMesh = new THREE.Mesh(star, starMaterial);
+    starMesh.position.set(positions[0], positions[1], positions[2]);
+    return starMesh;
+}
+  
+Array.from({length: 1000}, () => 1).map(() => {
+    scene.add( generateStar() );
+});
+
+const moonLoader = new THREE.TextureLoader();
+
+var moon;
+
+var rMoon = 35;
+var thetaMoon = 0;
+var dThetaMoon = 2 * Math.PI / 1000;
+
+const earthTexture = new THREE.TextureLoader().load(EarthTexture);
+
+
+const earthGeo = new THREE.SphereGeometry( 10, 50, 50 );
+const earthMat = new THREE.MeshBasicMaterial({map: earthTexture});
+const earth = new THREE.Mesh(earthGeo, earthMat);
+earth.position.set(0, 0, 0);
+scene.add(earth);
+
+const earthCloudTexture = new THREE.TextureLoader().load(EarthCloud);
+const cloudGeo = new THREE.SphereGeometry( 10.2, 50, 50 );
+const cloudMat = new THREE.MeshPhongMaterial({ 
+    map: earthCloudTexture,
+    transparent: true,
+    opacity: 0.1,
+    emissive: 0xffffff
+});
+const earthCloud = new THREE.Mesh(cloudGeo, cloudMat);
+earthCloud.position.set(0, 0, 0);
+scene.add(earthCloud)
+
+moonLoader.load(
+  MoonTexture,
+  function ( texture ) {
+      const moonGeometry = new THREE.SphereGeometry( 2.5, 50, 50 );
+      const moonMaterial = new THREE.MeshPhongMaterial( { 
+          map: texture,
+      } );
+      moon = new THREE.Mesh(moonGeometry, moonMaterial);
+      moon.position.set(35, 0, 0);
+      scene.add(moon)
+  },
+  undefined,
+  function ( err ) {
+    console.error( 'An error happened.' );
+  }
+);
+
+
+// Lights option
+
+// Point Light
+
+
+// Spot Light
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+directionalLight.visible = false;
+scene.add( directionalLight );
+
+// Debug
+const gui = new dat.GUI();
+
+/*
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
+
+
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.1, 1000);
+camera.position.set( 20, 20, 50 );
+scene.add(camera);
+
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+    // Update objects
+    if (earth instanceof THREE.Mesh) {
+        earth.rotation.y -= 0.002;
+      }
+    
+      if (earthCloud instanceof THREE.Mesh) {
+        earthCloud.rotation.y -= 0.0009;
+      }
+    
+      if (moon instanceof THREE.Mesh) {
+        thetaMoon += dThetaMoon;
+        moon.position.x = rMoon * Math.cos(thetaMoon);
+        moon.position.z = rMoon * Math.sin(thetaMoon);
+        moon.position.y = rMoon * Math.sin(thetaMoon);
+        moon.rotation.y -= 0.002;
+        moon.rotation.z -= 0.002;
+      }
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
